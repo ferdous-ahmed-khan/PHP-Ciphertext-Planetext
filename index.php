b2bb1eedb19efc8bbed846c275c132cd79ed9a8f
Ferdous Ahmed Khan <49646484+ferdousahmmed@users.noreply.github.com>
2022-01-25 16:31:21 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $name = "Ferdous Ahmed Khan";
 $email = "ferdous@nassa.com.bd";
 
-$plaintext = $name."-".$email."-";
+$plaintext = $name."-".$email;
 
 
 $cipher = "aes-128-gcm";
@@ -32,6 +32,11 @@ if (in_array($cipher, openssl_get_cipher_methods()))
  	
  	echo "<br><br>";
  
+
+
+
+/*
+
 	for($i = 0; $i < count($arr1); $i++) 
 		{
 
@@ -43,18 +48,34 @@ if (in_array($cipher, openssl_get_cipher_methods()))
 		}
 
 
-	for($i; $i < count($arr1); $i++) 
-		{
-			if($arr1[$i] == "-")
-			{	
-				break;
-				$email = $email."".$arr1[$i];
 
-			} 
+	$temp = count($arr1) + $i;
+
+
+	echo "Temp value: ".$temp[1]."<br><br>";
+
+
+
+	for($i = $i + 1; $i < $temp; $i++) 
+		{
+				$email = $email."".$temp[$i];
 		}
 
 
 		echo $name."<br>";	
 		echo $email;	
+*/
+
+
+	 
+ 
+
+		list($get_name, $get_mail) = explode('-', $original_plaintext);
+
+		echo $get_name."<br><br>";
+		echo $get_mail."<br><br>";
+
+
+ 
 
 ?>
